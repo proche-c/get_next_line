096ad222fe7df350fd1abbfd48f08f2b9bfd1ea4
proche-c <93194447+proche-c@users.noreply.github.com>
2022-03-23 17:12:06 +0100

    Add files via upload

---
+++ get_next_line_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: proche-c <proche-c@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/09 19:04:07 by proche-c          #+#    #+#             */
+/*   Updated: 2022/01/25 12:11:31 by proche-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+char	*ft_get_remaining(int fd, char *remaining);
+char	*ft_update_remaining(char *remaining);
+char	*ft_get_the_line(char *remaining);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *str);
+char	*ft_join_str(char *s1, char *s2);
+
+#endif
